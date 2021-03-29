408187112bf29e74682c67b1383989c2cf4f20e2
ChiatanyaChakka <73595822+ChiatanyaChakka@users.noreply.github.com>
2021-03-30 01:15:16 +0530

    Added(Pasted) logo onto splash screen

--- SplashScreen.py
+++ SplashScreen.py
@@ -6,21 +6,29 @@ width = splash.winfo_screenwidth()
 height = splash.winfo_screenheight()
 splash.state("zoomed")
 splash.title("Splash Screen")
+splash.overrideredirect(True)
 
 img_raw = Image.open('Images\SplashScreen.jpeg')
+raw_logo = Image.open('Images\DBMS_LOGO.png')
+raw_logo = raw_logo.resize((300,300), Image.ANTIALIAS)
 img_raw = img_raw.resize((width, height), Image.ANTIALIAS)
 img_raw = img_raw.filter(ImageFilter.GaussianBlur(3))
+img_raw.paste(raw_logo, ((width//3)+120, 300), raw_logo)
 img = ImageTk.PhotoImage(img_raw)
-
-
 panel = Label(splash, image=img)
-panel.pack(side="bottom", fill="both",
-           expand="yes")
+panel.place(x=0, y=0, relwidth=1, relheight=1)
+
+
+
+#logo = ImageTk.PhotoImage(raw_logo)
+#canvas = Canvas(splash, width = 200, height = 300)
+#canvas.create_image(0, 0, anchor=NW, image=logo)
+#canvas.pack(pady=300)
 
 def nextPage():
-    panel.forget()
+    splash.destroy()
     pass
 
-splash.after(2000, nextPage)
+splash.after(3000, nextPage)
 
 mainloop()
