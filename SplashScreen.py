14ef2374d526cfba90e7e8f828aa78a01e517ed6
ChiatanyaChakka <73595822+ChiatanyaChakka@users.noreply.github.com>
2021-04-01 23:58:18 +0530

    Added login screen file and pushed splash screen into a sepeate file.

--- SplashScreen.py
+++ SplashScreen.py
@@ -1,34 +1,22 @@
 from tkinter import *
 from PIL import ImageTk, Image, ImageFilter
 
-splash = Tk()
-width = splash.winfo_screenwidth()
-height = splash.winfo_screenheight()
-splash.state("zoomed")
-splash.title("Splash Screen")
-splash.overrideredirect(True)
+class splashScreenFrames:
+    def __init__(self, master):
+        self.splash = Frame(master)
 
-img_raw = Image.open('Images\SplashScreen.jpeg')
-raw_logo = Image.open('Images\DBMS_LOGO.png')
-raw_logo = raw_logo.resize((300,300), Image.ANTIALIAS)
-img_raw = img_raw.resize((width, height), Image.ANTIALIAS)
-img_raw = img_raw.filter(ImageFilter.GaussianBlur(3))
-img_raw.paste(raw_logo, ((width//3)+120, 300), raw_logo)
-img = ImageTk.PhotoImage(img_raw)
-panel = Label(splash, image=img)
-panel.place(x=0, y=0, relwidth=1, relheight=1)
+        # creating background image
+        img_raw = Image.open('Images\SplashScreen.jpeg')
+        raw_logo = Image.open('Images\DBMS_LOGO.png')
+        raw_logo = raw_logo.resize((300, 300), Image.ANTIALIAS)
+        img_raw = img_raw.resize((master.winfo_screenwidth(), master.winfo_screenheight()), Image.ANTIALIAS)
+        img_raw = img_raw.filter(ImageFilter.GaussianBlur(3))
+        img_raw.paste(raw_logo, ((master.winfo_screenwidth() // 3) + 120, 300), raw_logo)
+        # done creating background image
+        self.img = ImageTk.PhotoImage(img_raw)
+        panel = Label(self.splash, image=self.img)
+        panel.pack()
+        self.splash.pack()
 
-
-
-#logo = ImageTk.PhotoImage(raw_logo)
-#canvas = Canvas(splash, width = 200, height = 300)
-#canvas.create_image(0, 0, anchor=NW, image=logo)
-#canvas.pack(pady=300)
-
-def nextPage():
-    splash.destroy()
-    pass
-
-splash.after(3000, nextPage)
-
-mainloop()
+    def forgetframe(self):
+        self.splash.pack_forget()
