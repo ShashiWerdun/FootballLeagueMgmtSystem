c63e69b5fa874c599bb7529e220d76ae4344d564
K.S.V.L.Druthi <f20190023@hyderabad.bits-pilani.ac.in>
2021-04-05 10:20:06 +0530

    reg_screen added

--- LoginScreen.py
+++ LoginScreen.py
@@ -6,18 +6,14 @@ class loginScreenFrame:
 
     def __init__(self, master):
         self.login_frame = Frame(master)
-        img_raw = Image.open('Images\SplashScreen_copy.jpeg')
+        img_raw = Image.open('Images\SplashScreen.jpeg')
         img_raw = img_raw.resize((master.winfo_screenwidth(), master.winfo_screenheight()), Image.ANTIALIAS)
         img_raw = img_raw.filter(ImageFilter.GaussianBlur(3))
         self.photo = ImageTk.PhotoImage(img_raw)
         self.pic = Label(self.login_frame, image=self.photo)
-        #self.image.place(x=0, y=0, relwidth=1, relheight=1)
         self.pic.pack()
         self.frame_login = Frame(self.login_frame, bg="White")
         self.frame_login.place(x=525, y=300, width=550, height=300)
-       #self.frame_login.pack()
-        # self.hello = Label(self.login_frame, text="Black")
-        # self.hello.pack()
         title = Label(self.frame_login, text="Login Screen", font=("Verdana", 16, "bold")).place(x=200, y=20)
         user_lbl = Label(self.frame_login, text="UserName", font=("Goudy old style", 13)).place(x=70, y=75)
         self.txt_usr = Entry(self.frame_login, font=("times new roman", 12)).place(x=170, y=75)
@@ -28,6 +24,5 @@ class loginScreenFrame:
         nwfan_login = Button(self.frame_login, text="New Fan?|Signup Here!", font=("Goudy old style", 9)).place(x=50, y=230)
         pwd_lbl = Label(self.frame_login, text="Contact us at:***********", font=("times new roman", 8)).place(x=375, y=275)
         self.login_frame.place(x=0, y=0, relwidth=1, relheight=1)
-        #self.image.place(x=0,y=0,relwidth=1,relheight=1)
 
 
